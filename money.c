92015a07e067fe5c314b9857b0e7c9041deca74a
flarn2006 <flarn2006@gmail.com>
2013-04-01 23:06:53 -0300

    Made money.c look better

--- money.c
+++ money.c
@@ -36,26 +36,16 @@ int main(int argc, char *argv[])
 	printf("\n");
 	printf("$%.2lf is equal to:\n\n", original);
 
-	if (bills100)
-		printf("%d $100 bills\t= $%.2lf\n", bills100, bills100 * 100.00);
-	if (bills50)
-		printf("%d $50 bills\t= $%.2lf\n", bills50, bills50 * 50.00);
-	if (bills20)
-		printf("%d $20 bills\t= $%.2lf\n", bills20, bills20 * 20.00);
-	if (bills10)
-		printf("%d $10 bills\t= $%.2lf\n", bills10, bills10 * 10.00);
-	if (bills5)
-		printf("%d $5 bills\t= $%.2lf\n", bills5, bills5 * 5.00);
-	if (bills1)
-		printf("%d $1 bills\t= $%.2lf\n", bills1, (double)bills1);
-	if (quarters)
-		printf("%d quarters\t= $%.2lf\n", quarters, quarters * 0.25);
-	if (dimes)
-		printf("%d dimes\t\t= $%.2lf\n", dimes, dimes * 0.10);
-	if (nickels)
-		printf("%d nickels\t= $%.2lf\n", nickels, nickels * 0.05);
-	if (pennies)
-		printf("%d pennies\t= $%.2lf\n", pennies, pennies * 0.01);
+	if (bills100)	printf("%d $100 bills\t= $%.2lf\n", bills100, bills100 * 100.00);
+	if (bills50)	printf("%d $50 bills\t= $%.2lf\n", bills50, bills50 * 50.00);
+	if (bills20)	printf("%d $20 bills\t= $%.2lf\n", bills20, bills20 * 20.00);
+	if (bills10)	printf("%d $10 bills\t= $%.2lf\n", bills10, bills10 * 10.00);
+	if (bills5)	printf("%d $5 bills\t= $%.2lf\n", bills5, bills5 * 5.00);
+	if (bills1)	printf("%d $1 bills\t= $%.2lf\n", bills1, (double)bills1);
+	if (quarters)	printf("%d quarters\t= $%.2lf\n", quarters, quarters * 0.25);
+	if (dimes)	printf("%d dimes\t\t= $%.2lf\n", dimes, dimes * 0.10);
+	if (nickels)	printf("%d nickels\t= $%.2lf\n", nickels, nickels * 0.05);
+	if (pennies)	printf("%d pennies\t= $%.2lf\n", pennies, pennies * 0.01);
 	
 	printf("\n");
 
