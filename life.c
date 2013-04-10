f272d4b3b7a20ee0af84e2634c04de7fa7d2b850
flarn2006 <flarn2006@gmail.com>
2013-04-10 05:05:27 -0400

    life.c can now load from a file (pass filename as command line parameter)

--- life.c
+++ life.c
@@ -1,47 +1,105 @@
 #include <curses.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define BV(x,y) ((field[x][y] == '#' || field[x][y] == 'X') ? 1:0);
+#define WIDTH 30
+#define HEIGHT 20
 
-char field[30][20];
+char field[WIDTH][HEIGHT];
 
 int neighbors(int x, int y)
 {
 	int count = 0;
-	if (x > 0 && y > 0)		count += BV(x-1,y-1);
-	if (y > 0)				count += BV(x,y-1);
-	if (x < 29 && y > 0)	count += BV(x+1,y-1);
-	if (x > 0)				count += BV(x-1,y);
-	if (x < 29)				count += BV(x+1,y);
-	if (x > 0 && y < 19)	count += BV(x-1,y+1);
-	if (y < 19)				count += BV(x,y+1);
-	if (x < 29 && y < 19)	count += BV(x+1,y+1);
+	if (x > 0 && y > 0)					count += BV(x-1,y-1);
+	if (y > 0)							count += BV(x,y-1);
+	if (x < WIDTH-1 && y > 0)			count += BV(x+1,y-1);
+	if (x > 0)							count += BV(x-1,y);
+	if (x < WIDTH-1)					count += BV(x+1,y);
+	if (x > 0 && y < HEIGHT-1)			count += BV(x-1,y+1);
+	if (y < HEIGHT-1)					count += BV(x,y+1);
+	if (x < WIDTH-1 && y < HEIGHT-1)	count += BV(x+1,y+1);
 	return count;
 }
 
-int main (int argc, const char * argv[])
+int loadWorld(const char *filename, char field[][HEIGHT], const char *argv0)
+{
+	int y;
+	FILE *fp = fopen(filename, "r");
+	if (!fp)
+	{
+		perror(argv0);
+		return 2;
+	}
+
+	memset(field, ' ', WIDTH*HEIGHT);
+	
+	for (y=0; y<HEIGHT; y++)
+	{
+		int x, length;
+		char line[WIDTH+1];
+
+		fgets(line, WIDTH, fp);
+		if (feof(fp)) break;
+		length = strlen(line);
+		
+		for (x=0; x<length; x++)
+		{
+			char ch = line[x];
+			if (ch == ' ' || ch == '#') field[x][y] = ch;
+			else if (ch == '\n') break;
+			else
+			{
+				fprintf(stderr, "Unknown character '%c' found.\n", ch);
+				return 1;
+			}
+		}
+	}
+	
+	fclose(fp);
+	
+	return 0;
+}
+				
+
+int main(int argc, const char *argv[])
 {
 	int x, y;
 	int gen = 0;
-	initscr();
-	cbreak();
-	noecho();
-
-	srand((unsigned)time(NULL));
+	int result = 0;
 	
-	for (y=0; y<20; y++)
-		for (x=0; x<30; x++)
-		{
-			field[x][y] = rand() % 2 ? '#':' ';
-		}
+	switch (argc)
+	{
+		case 1:
+			srand((unsigned)time(NULL));
+			for (y=0; y<HEIGHT; y++)
+			for (x=0; x<WIDTH; x++)
+			{
+				field[x][y] = rand() % 2 ? '#':' ';
+			}
+			break;
+			
+		case 2:
+			result = loadWorld(argv[1], field, argv[0]);
+			if (result) return result;
+			break;
+			
+		default:
+			printf("Usage: %s [filename]\n", argv[0]);
+			return 3;
+			break;
+	}
 	
+	initscr(); cbreak(); noecho();
+			
 	for (;;)
 	{
 		clear();
 		mvprintw(22, 0, "Generation: %d", gen);
-		for (y=0; y<20; y++)
-			for (x=0; x<30; x++)
+		for (y=0; y<HEIGHT; y++)
+			for (x=0; x<WIDTH; x++)
 			{
 				if (field[x][y] == '#') mvprintw(y, x*2, "[]");
 			}
@@ -50,8 +108,8 @@ int main (int argc, const char * argv[])
 		
 		gen ++;
 		
-		for (y=0; y<20; y++)
-			for (x=0; x<30; x++)
+		for (y=0; y<HEIGHT; y++)
+			for (x=0; x<WIDTH; x++)
 			{
 				if (neighbors(x,y) == 3 && field[x][y] == ' ')
 					field[x][y] = '+';
@@ -59,8 +117,8 @@ int main (int argc, const char * argv[])
 					field[x][y] = 'X';
 			}
 		
-		for (y=0; y<20; y++)
-			for (x=0; x<30; x++)
+		for (y=0; y<HEIGHT; y++)
+			for (x=0; x<WIDTH; x++)
 			{
 				if (field[x][y] == 'X') field[x][y] = ' ';
 				if (field[x][y] == '+') field[x][y] = '#';
