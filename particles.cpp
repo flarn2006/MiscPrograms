89599a902d91de0bf61a8d0ea0183c3524e547c9
flarn2006 <flarn2006@gmail.com>
2013-08-02 00:30:16 -0400

    Made several more changes to particles.cpp

--- particles.cpp
+++ particles.cpp
@@ -5,9 +5,10 @@
 #include "delayms.h"
 
 #define TIME_STEP 0.01f
-#define MAX_PARTICLES 256
+#define MAX_PARTICLES 4096
 #define VEL_INCREMENT 0.5f
 #define ACCEL_INCREMENT 2.0f
+#define AGE_INCREMENT 0.2f
 
 class Particle
 {
@@ -62,7 +63,8 @@ int main(int argc, char *argv[])
 	float velocity = 10.0f;
 	float accelX = 0.0f;
 	float accelY = 0.0f;
-	int displayOn = 1;
+	float maxAge = 1.0f;
+	char display = 3;
 	
 	std::vector<Particle> *psys = new std::vector<Particle>();
 	
@@ -71,6 +73,7 @@ int main(int argc, char *argv[])
 	noecho();
 	keypad(stdscr, TRUE);
 	nodelay(stdscr, TRUE);
+	curs_set(0);
 	getmaxyx(stdscr, ymax, xmax);
 
 	originX = (float)(xmax+1) / 4.0f;
@@ -82,13 +85,6 @@ int main(int argc, char *argv[])
 	init_pair(3, 1, 0); //red
 	init_pair(4, 6, 0); //cyan
 
-	erase();
-	printw("Use the arrow keys to move, and press 'q' to stop...");
-	refresh();
-	delayms(1000);
-
-	curs_set(0);
-
 	int quitNow = 0;
 	while (!quitNow) {
 		erase();
@@ -97,38 +93,46 @@ int main(int argc, char *argv[])
 		for (i=psys->begin(); i!=psys->end(); i++) {
 			i->simulate(TIME_STEP);
 			float age = i->getAge();
-			int pair;
 			
-			if (age < 0.75f) {
-				pair = 1;
-			} else if (age < 1.5f) {
-				pair = 2;
-			} else {
-				pair = 3;
-			}
+			if (age <= maxAge) {
+				int pair;
+				if (age < maxAge * 0.33f) {
+					pair = 1;
+				} else if (age < maxAge * 0.66f) {
+					pair = 2;
+				} else {
+					pair = 3;
+				}
 
-			attron(COLOR_PAIR(pair));
-			mvprintw((int)i->y, (int)i->x * 2, "()");
-			attroff(COLOR_PAIR(pair));
+				attron(COLOR_PAIR(pair));
+				mvprintw((int)i->y, (int)i->x * 2, "()");
+				attroff(COLOR_PAIR(pair));
+			}
 
 			i->setAccel(accelX, accelY);
 		}
 
 		Particle p(originX, originY, velocity);
-		psys->insert(psys->begin(), p);
+		psys->push_back(p);
 
 		if (psys->size() > MAX_PARTICLES) {
-			psys->pop_back();
+			psys->erase(psys->begin());
 		}
 
-		if (displayOn) {
-			attron(A_BOLD); attron(COLOR_PAIR(4));
+		attron(A_BOLD); attron(COLOR_PAIR(4));
+		if (display & 1) {
 			mvprintw((int)originY, (int)originX * 2, "<>");
-			mvprintw(0, 0, "[-/+] Max Velocity:   %.1f", velocity);
-			mvprintw(1, 0, "[A/D] X Acceleration: %.0f", accelX);
-			mvprintw(2, 0, "[W/S] Y Acceleration: %.0f", accelY);
-			attroff(A_BOLD); attroff(COLOR_PAIR(4));
 		}
+		if (display & 2) {
+			mvprintw(0, 0, "<Arrows> Move emitter");
+			mvprintw(1, 0, "<-/+> Max Velocity:   %.1f", velocity);
+			mvprintw(2, 0, "<A/D> X Acceleration: %.0f", accelX);
+			mvprintw(3, 0, "<W/S> Y Acceleration: %.0f", accelY);
+			mvprintw(4, 0, "<[/]> Maximum age:    %.1f", maxAge);
+			mvprintw(5, 0, "<X>   Clear all particles");
+			mvprintw(6, 0, "<`>   Change display");
+		}
+		attroff(A_BOLD); attroff(COLOR_PAIR(4));
 
 		refresh();
 		delayms(1000 * TIME_STEP);
@@ -146,8 +150,12 @@ int main(int argc, char *argv[])
 			case 'd': accelX += ACCEL_INCREMENT; break;
 			case 'w': accelY -= ACCEL_INCREMENT; break;
 			case 's': accelY += ACCEL_INCREMENT; break;
+
+			case '[': maxAge -= AGE_INCREMENT; if (maxAge < 0) maxAge = 0; break;
+			case ']': maxAge += AGE_INCREMENT; break;
 			
-			case '`': displayOn = !displayOn; break;
+			case 'x': psys->clear(); break;
+			case '`': display ++; break;
 			case 'q': quitNow = 1; break;
 		}
 	}
