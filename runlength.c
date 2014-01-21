fce1decec82ca70f6cf7912002a2d1e000e1e4d8
flarn2006 <flarn2006@gmail.com>
2014-01-21 15:18:37 -0500

    runlength.c now handles multiple args correctly

--- runlength.c
+++ runlength.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *catArgs(int argc, char *argv[])
+{
+	size_t length = 0;
+	int i; for (i=1; i<argc; i++) {
+		length += strlen(argv[i]);
+		if (i < argc - 1) length++; // space
+	}
+
+	char *buf = malloc(length + 1);
+	*buf = '\0';
+	for (i=1; i<argc; i++) {
+		strcat(buf, argv[i]);
+		if (i < argc - 1) strcat(buf, " ");
+	}
+
+	return buf;
+}
 
 int main(int argc, char *argv[]) 
 {
-	char *ptr;
+	char *ptr, *buf;
 	unsigned int runlength = 0;
 	
-	if (argc != 2) {
+	if (argc < 2) {
 		printf("Usage: %s string\n", argv[0]);
 		return 1;
 	}
 
-	ptr = argv[1];
+	ptr = buf = catArgs(argc, argv);
+	
 	while (*ptr != '\0') {
 		static char last;
 
-		if (ptr == argv[1]) /*if first character*/ last = *ptr;
+		if (ptr == buf) /*if first character*/ last = *ptr;
 		
 		if (*ptr == last) runlength ++;
 		else {
@@ -23,10 +44,11 @@ int main(int argc, char *argv[])
 			last = *ptr;
 		}
 
-		ptr ++;
+		ptr++;
 	}
 
 	printf("%u\tx '%c'\n", runlength, *(ptr-1));
 
+	free(buf);
 	return 0;
 }
