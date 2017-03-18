e067bc3fa8c5b4107e47ea0d55fa14176b0fa456
flarn2006 <flarn2006@gmail.com>
2017-03-18 14:14:53 -0400

    Added a scary warning to the beginning of exclude.c

--- exclude.c
+++ exclude.c
@@ -2,11 +2,16 @@
  * Useful for shell wildcards.
  * Usage: exclude [args...] [- [blacklist...]]
  *   e.g. exclude ab cd ef gh ij - cd gh  ->  ab ef ij
- * Example: Delete all files except for .jp(e)g files
- *   rm `exclude "*" - "*.jpg" "*.jpeg"`
- * NOTE: quotes around wildcards are important in case filenames have spaces!
+ * Example: Move all files except for .jp(e)g files to a directory
+ *   mv `exclude "*" - "*.jpg" "*.jpeg"` non_jpegs/
+ * NOTE: Quoting wildcards is important because of the way `` works!
+ *
+ * WARNING: DO NOT use `exclude` for anything (like rm) that can't be undone!
+ *          I have encountered issues before where it doesn't exclude like it
+ *          should, and I haven't figured out what the cause is yet.
  */
 
+#warning "[1;5;7;33m READ THE COMMENT AT THE TOP OF EXCLUDE.C BEFORE USING IT! [m"
 #include <stdio.h>
 #include <string.h>
 
