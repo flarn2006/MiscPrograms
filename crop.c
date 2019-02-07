d29a0015d2735a862c2fcc9657952315822a177e
flarn2006 <flarn2006@gmail.com>
2019-02-06 19:12:15 -0500

    fix: improve crop.c with bugfixes

--- crop.c
+++ crop.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-typedef enum { BEFORE_PREFIX, AT_TARGET, AFTER_SUFFIX } state_t;
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
+	int exit_status = 0;
 	const char *filename;
 	FILE *fp;
 	if (argc == 4) {
 		filename = argv[3];
 		fp = fopen(filename, "r");
 		if (!fp) {
-			perror(filename);
+			fprintf(stderr, "%s: cannot access '%s': %s\n", argv[0], filename, strerror(errno));
 			return 1;
 		}
 	} else if (argc == 3) {
@@ -29,16 +29,37 @@ int main(int argc, char *argv[])
 
 	size_t bufsize = 1024;
 	char *buf = malloc(bufsize);
-	state_t state = BEFORE_PREFIX;
+	if (!buf) {
+		fprintf(stderr, "%s: Out of memory\n", argv[0]);
+		exit_status = 2;
+		goto err_close_file;
+	}
+
+	setbuf(stdout, NULL);  /* for easier error handling */
 
 	while (!feof(fp)) {
-		fgets(buf, bufsize, fp);
+		if (fgets(buf, bufsize, fp) == NULL) {
+fgets_null_handler:
+			if (errno) {
+				fprintf(stderr, "%s: error reading '%s': %s\n", argv[0], filename, strerror(errno));
+				exit_status = 1;
+				goto err_free_and_close;
+			} else {
+				break;
+			}
+		}
 		size_t len = strlen(buf);
 		while (len == bufsize-1 && buf[len-1] != '\n') {
 			size_t old_bufsize = bufsize;
 			bufsize *= 2;
 			buf = realloc(buf, bufsize);
-			fgets(buf+len-1, old_bufsize, fp);
+			if (!buf) {
+				fprintf(stderr, "%s: Out of memory\n", argv[0]);
+				exit_status = 2;
+				goto err_free_and_close;
+			}
+			if (fgets(buf+len-1, old_bufsize, fp) == NULL)
+				goto fgets_null_handler;  /* It's okay, don't worry. Dinosaurs are extinct. */
 			len = strlen(buf);
 		}
 
@@ -46,7 +67,10 @@ int main(int argc, char *argv[])
 		if (pfxstart) {
 			const char *tgtstart = pfxstart + prefix_len;
 			size_t tgt_len;
-			const char *sfxstart = strstr(tgtstart, suffix);
+			const char *sfxstart = NULL;
+			if (*suffix)
+				sfxstart = strstr(tgtstart, suffix);
+			
 			int already_did_newline = 0;
 			if (sfxstart) {
 				tgt_len = sfxstart - tgtstart;
@@ -54,13 +78,19 @@ int main(int argc, char *argv[])
 				tgt_len = len - (tgtstart - buf);
 				already_did_newline = 1;
 			}
-			fwrite(tgtstart, 1, tgt_len, stdout);
-			if (!already_did_newline)
+			if (fwrite(tgtstart, 1, tgt_len, stdout) < tgt_len) {
+				fprintf(stderr, "%s: write error: %s\n", argv[0], strerror(errno));
+				return 1;
+			} else if (!already_did_newline) {
 				putchar('\n');
+			}
 		}
 	}
 
-	fclose(fp);
+err_free_and_close:
 	free(buf);
-	return 0;
+err_close_file:
+	if (fp != stdin)
+		fclose(fp);
+	return exit_status;
 }
